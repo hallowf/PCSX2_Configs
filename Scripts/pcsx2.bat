72fb872edc90b71d12152464a3fac944d6e7a571
hallowf <alexandre.cortegaca@gmail.com>
2019-02-15 23:20:18 +0000

    Changed pcsx2.exe call to variable

--- Scripts/pcsx2.bat
+++ Scripts/pcsx2.bat
@@ -2,5 +2,5 @@ REM SET REGISTRY ENTRIES FOR CUSTOMIZED PCSX2
 start regedit.exe /s "%GAME_CONFIG%\pcsx2.reg"
 
 REM START PCSX2
-cd %GAME_CONFIG%
-start pcsx2.exe "%GAME_ISO%"
+echo game config %GAME_CONFIG%
+start %GAME_CONFIG%\pcsx2.exe "%GAME_ISO%"
